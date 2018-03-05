f0ead610f8d922542408122a583d9978db2608de
gustav <kravitzluis@gmail.com>
2018-03-05 00:44:20 -0700

    LP-Front Se hace la conexion al api y se muestran las listas

--- client_react/src/components/list.js
+++ client_react/src/components/list.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
 import './../App.css';
-
+const api = 'http://localhost:3000/api/lists';
 class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [],
+    };
+  }
+  componentDidMount() {
+    this.getList();
+  }
+  getList() {
+    console.log('get list');
+    fetch(api)
+      .then(response => {
+        response.json().then(json => {
+          console.log(json);
+          this.setState({
+            list: json,
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    const dataList = this.state.list.map((list, i) => {
+      return (
+        <li key={i} className="collection-item">
+          {' '}
+          {list.name}
+        </li>
+      );
+    });
     return (
       <div>
-        <h1>Lisstas</h1>
+        <ul className="collection">{dataList}</ul>
       </div>
     );
   }
